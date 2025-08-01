3ee72b35ccc65a3eb64cfb2e708bae80601aac1a
Lucas Jacquemin <lucas.jacquemin2002@gmail.com>
2025-08-01 18:50:01 +0200

    Added images for 42seconds, Aimcore, Cub3d, Fractol, Shader, Malloc, printf, Elements Keeper

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -19,6 +19,23 @@ const portfolioDescription = (
 	  </a>.
 	</p>
   )
+  const shaderDescription = (
+	  <p className="text-sm text-gray-400">
+		ShaderPixel is a real-time GPU graphics project developed using Rust, Vulkan, and GLSL. 
+		It features advanced 3D shaders that render complex fractals, volumetric raymarching effects, 
+		and dynamic lighting entirely on the GPU. The project offers an interactive 3D environment to explore 
+		a variety of shader-based visual effects such as translucent materials, volumetric clouds with shadows, 
+		and detailed fractal objects like mandelboxes. Some of the shader I made are visible on {' '}
+		<a
+		  href="https://www.shadertoy.com/user/ejacquem"
+		  target="_blank"
+		  rel="noopener noreferrer"
+		  className="underline text-blue-400 hover:text-blue-300"
+		>
+		  my ShaderToy account
+		</a>.
+	  </p>
+	)
 
 enum ProjectType {
 	School = '42 Project',
@@ -55,7 +72,7 @@ const projects = [
 		descriptionLong: 'Aimcore is a fast-paced multiplayer game about surviving waves of enemies with increasing difficulty.\
 		The game is built with the Unity engine and implements various AI algorithms for the enemies, a main menu, visual and sound effects and a highscore system.\
 		We used Fusion 2 for the networking wich helped us implement lag compensation methdods to ensure smooth gameplay.',
-		slides: [],
+		slides: ['Aimcore_Menu.png', 'Aimcore_Game.png', 'Aimcore_GameOver.png'],
 		link: 'https://siul008.itch.io/aimcore',
 		tech: ['C#', 'Unity', 'Blender', 'Audacity', 'Photon', 'Fusion 2'],
 		teamSize: 2,
@@ -66,7 +83,7 @@ const projects = [
 		description: 'Tester For The 42 Project ft_printf ',
 		descriptionLong: 'The project ft_print requires the recreation of the C function prtinf(). The tester will take the output of the custom printf funcition\
 		and compare the result with the real function. It will print in the terminal the result of the comparison in a clean and readable format.',
-		slides: ['code.jpg'],
+		slides: ['Printf_Tester.png'],
 		link: 'https://github.com/ejacquem/printfBetterTester',
 		tech: ['C','Makefile','Unit Test'],
 		teamSize: 1,
@@ -74,10 +91,10 @@ const projects = [
 	{
 		title: 'Cub3D',
 		type: ProjectType.School,
-		description: 'Raycasting-Based 3D Engine Inspired by Wolfenstein',
+		description: 'MultiThreading Raycasting-Based 3D Engine',
 		descriptionLong: 'Cub3D is a project to create a simple 3D engine in C using raycasting. It transforms a 2D map into a 3D perspective from a first-person view. \
 		The goal is to navigate a maze-like map and find the exit while avoiding enemies.',
-		slides: ['pong.gif'],
+		slides: ['Cub3d.gif', 'Cub3d.png', 'Cub3d_Menu.png'],
 		link: '',
 		tech: ['C', 'Minilibx'],
 		teamSize: 2,
@@ -99,7 +116,7 @@ const projects = [
 		description: 'Puzzle Game Made in 2 Week for LGX Game Jam',
 		descriptionLong: '42 Seconds is a fast-paced puzzle game developed for the LGX game jam in under one week. \
 		The player must solve short puzzles under a strict 42-second time limit. Built with Unity, it emphasizes intuitive design, accessibility, and polished user experience.',
-		slides: [],
+		slides: ['42Seconds.png'],
 		link: 'https://ejacquem.itch.io/42seconds',
 		tech: ['C#', 'Unity', 'Blender', 'Audacity'],
 		teamSize: 6,
@@ -111,7 +128,7 @@ const projects = [
 		descriptionLong: 'Elements Keeper was built during a 72-hour game jam with the theme "Shadow and Alchemy". \
 		It\'s a tower defense game where players mix elements to create towers that defend against waves of enemies. \
 		The game won #1 in Playability.',
-		slides: [],
+		slides: ['EK_Menu.png', 'EK_Game.png', 'EK_Craft.png', ],
 		link: 'https://ejacquem.itch.io/elements-keeper',
 		tech: ['C#', 'Unity', 'Krita', 'Audacity', 'FL Studio'],
 		teamSize: 3,
@@ -160,8 +177,8 @@ const projects = [
 		It involves designing and building a custom dynamic memory allocator that efficiently \
 		handles allocation, deallocation, and fragmentation. The project focuses on implementing \
 		malloc, free, and realloc functions, focusing on memory efficiency and performance optimization.',
-		slides: [],
-		link: '',
+		slides: ['Malloc_Terminal.png'],
+		link: 'https://github.com/ejacquem/malloc',
 		tech: ['C', 'Makefile'],
 		teamSize: 1,
 	},
@@ -174,10 +191,11 @@ const projects = [
 		and dynamic lighting entirely on the GPU. The project offers an interactive 3D environment to explore \
 		a variety of shader-based visual effects such as translucent materials, volumetric clouds with shadows, \
 		and detailed fractal objects like mandelboxes',
-		slides: [],
+		slides: ['Shader_Truchet.gif', 'Shader_Gem.png', 'Shader_Mozaic.png'],
 		link: '',
 		tech: ['Rust', 'Vulkan', 'GLSL', 'Makefile'],
 		teamSize: 2,
+		descriptionComponent: shaderDescription,
 	},
 	{
 		title: 'Fractol',
@@ -187,11 +205,10 @@ const projects = [
 		and the MiniLibX library. The project focuses on generating and displaying complex \
 		mathematical structures like the Mandelbrot and Julia sets. It features real-time zooming, \
 		mouse and keyboard interactions, and custom parameter inputs to explore different fractal variants.',
-		slides: [],
+		slides: ['Fractol_BudhaColor.png', 'Fractol_1.png', 'Fractol_2.png', 'Fractol_3.png', 'Fractol_4.png', 'Fractol_BudhaBrot.png'],
 		link: '',
 		tech: ['C', 'MiniLibX', 'Makefile'],
 		teamSize: 1,
-		
 	},
 	{
 		title: '42 Luxembourg Leaderboard',
@@ -201,9 +218,9 @@ const projects = [
 		fetches real-time student data from the 42 school API and dynamically ranks users based on criteria \
 		such as experience level, total logtime, or evaluation points. The project provides an \
 		interactive interface for exploring and comparing student data.',
-		slides: [],
+		slides: ['42LeaderboardAnon.png'],
 		link: '',
-		tech: ['JavaScript', 'CSS', 'API'],
+		tech: ['JavaScript', 'CSS', 'HTML', 'API'],
 		teamSize: 2,
 	},
 	{
@@ -294,21 +311,30 @@ export default function ProjectsSection() {
 									)}
 								</div>
 
-								<div className="relative h-full min-h-[300px] lg:min-h-full">
 									{project.slides.length > 1 ? (
-										<div className="flex items-center justify-center h-full">
-											<EmblaCarousel slides={project.slides} options={{loop: true}}/>
+										<div className="relative h-full min-h-[300px] lg:min-h-full">
+											<div className="flex items-center justify-center h-full">
+												<EmblaCarousel slides={project.slides} options={{loop: true}}/>
+												<div className="pointer-events-none absolute inset-0 bg-[linear-gradient(to_bottom,_#0a0d18_0%,_transparent_10%,_transparent_90%,_#0a0d18_100%)] lg:bg-[linear-gradient(to_right,_#0a0d18_0%,_transparent_10%,_transparent_90%,_#0a0d18_100%)]"></div>
+											</div>
 										</div>
 									) : (
 										project.slides.length == 1 ? (
-											<Image src={project.slides[0]} alt={project.title} fill className="object-cover" sizes="(max-width: 1024px) 100vw, 50vw" />
+											<div className="flex items-center justify-center m-4">
+												<Image
+													src={project.slides[0]}
+													alt={project.title}
+													width={800} // or responsive size like 600
+													height={600}
+													className="object-contain max-w-full max-h-full rounded-lg"
+												/>
+											</div>
+											// <Image src={project.slides[0]} alt={project.title} fill className="object-cover" sizes="(max-width: 1024px) 100vw, 50vw" />
 										) :  (
 											<div className="embla__slide" key={index}></div>
 										)
 									)}
 								{/* <div className="pointer-events-none absolute inset-0 bg-gradient-to-b lg:bg-gradient-to-r from-[#0a0d18] via-transparent to-transparent lg:via-[#0a0d18]/20 lg:to-[#0a0d18]/40"></div> */}
-								<div className="pointer-events-none absolute inset-0 bg-[linear-gradient(to_bottom,_#0a0d18_0%,_transparent_10%,_transparent_90%,_#0a0d18_100%)] lg:bg-[linear-gradient(to_right,_#0a0d18_0%,_transparent_10%,_transparent_90%,_#0a0d18_100%)]"></div>
-								</div>
 							</div>
 						</div>
 					</motion.div>
